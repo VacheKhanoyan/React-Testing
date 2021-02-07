94db0c06c7eb0422ef81c4ef84e351e9bc8c96e0
VacheKhanoyan <khanoyann@gmail.com>
2021-02-07 23:54:31 +0400

    first commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transformIgnorePatterns: [
+      "./node_modules",
+    ],
+    moduleNameMapper: {
+        "\\.(css|sass)$": "identity-obj-proxy",
+      },
+  }
---
+++ src/jest.config.js
@@ -0,0 +1,3 @@
+// module.exports = {
+//     preset: '@xxx/rdk.jest-preset-proactive'
+//   };
